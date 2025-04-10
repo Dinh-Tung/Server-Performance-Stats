1b69eda1b29587a6c315d26c5c42ea3922c52766
tungdt <tungdt@yopaz.dev>
2025-04-10 14:43:29 +0700

    add file sh

---
+++ server-stats.sh
@@ -0,0 +1,10 @@
+echo "Total CPU usage"
+top -bn1 | grep "Cpu(s)"
+echo "Total memory usage (Free vs Used including percentage)"
+free -m
+echo "Total disk usage (Free vs Used including percentage)"
+df -h /
+echo "Top 5 processes by CPU usage"
+ps -eo pid,comm,%cpu
+echo "Top 5 processes by memory usage"
+ps -eo pid,comm,%mem
